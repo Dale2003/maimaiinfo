930f6dedab57a1598380cac7014e74526db70b5e
宇航员dale <dale2003@126.com>
2025-04-23 19:49:18 +0800

    first

---
+++ merge.py
@@ -0,0 +1,33 @@
+import json
+with open('static/music_data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+with open('static/all_alias.json', 'r', encoding='utf-8') as f:
+    alias = json.load(f)
+
+with open('static/music_chart.json', 'r', encoding='utf-8') as f:
+    chart = json.load(f)
+
+new_data = {}
+
+for music in data:
+    if music["id"] not in new_data:
+        new_data[music["id"]] = music
+
+for id in alias:
+    if id in new_data:
+        new_data[id]["alias"] = alias[id]
+    else:
+        print(f"ID {id} not found in music data.")
+
+for id in chart["charts"]:
+    new_data[id]["fit_diff"] = []
+    for c in chart["charts"][id]:
+        # print(c)
+        try:
+            new_data[id]["fit_diff"].append(c['fit_diff'])
+        except KeyError:
+            pass
+
+with open('static/all_data.json', 'w', encoding='utf-8') as f:
+    json.dump(new_data, f, ensure_ascii=False, indent=4)
