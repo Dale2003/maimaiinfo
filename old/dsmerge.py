9ec83ac9b423167ec345a0d133e3bb959204c70d
宇航员dale <2100012826@stu.pku.edu.cn>
2025-10-13 14:22:38 +0800

    1.55update

--- old/dsmerge.py
+++ old/dsmerge.py
@@ -6,6 +6,9 @@ with open("static/dschange.json", "r", encoding="utf-8") as f:
 with open('static/160ds.json', 'r', encoding='utf-8') as f:
     ds160 = json.load(f)
 
+with open('static/all_data_new.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
 for musicid in ds160:
     if str(musicid) in dschange:
         for lvidx, lv in enumerate(ds160[musicid]['ds']):
@@ -14,7 +17,15 @@ for musicid in ds160:
             else:
                 dschange[musicid]["ds"].append({'maimai DX CiRCLE': lv})
     else:
-        dschange[musicid] = {
+        if "CiRCLE" not in data[str(musicid)]["basic_info"]["from"]:
+            dschange[musicid] = {
+            'id': musicid,
+            'name': ds160[musicid]['name'],
+            'ds': [{'maimai DX PRiSM PLUS': lv,
+                    'maimai DX CiRCLE': lv} for lv in ds160[musicid]['ds']]
+            }
+        else:
+            dschange[musicid] = {
             'id': musicid,
             'name': ds160[musicid]['name'],
             'ds': [{'maimai DX CiRCLE': lv} for lv in ds160[musicid]['ds']]}
